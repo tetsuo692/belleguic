c5986c10feaadb224b59103d8a7676bc82915c95
Guillaume BELLEGUIC <guillaume.belleguic@oceans.eu>
2013-09-09 09:35:10 +0200

    start cv

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,8 +1,20 @@
-/* Author: 
+/* Author:
 
 */
 
+$(function(){
+  $('.avatar').fadeIn(3500).rotate({
+    angle: 0,
+    animateTo: 360,
+    duration: 4500,
+    callback: function(){
+      $('.description h2').show('slide', {direction: 'right'}, 300, function(){
+       $('.description p').show('slide', {direction: 'up'}, 200);
+      });
+    }
+  });
 
+});
 
 
 
