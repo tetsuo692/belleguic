78f729bce362f399fbf04f4c2cacc16c9128d88e
Guillaume BELLEGUIC <guillaume.belleguic@oceans.eu>
2013-09-09 12:28:52 +0200

    speed up animation

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -6,7 +6,7 @@ $(function(){
   $('.avatar').fadeIn(3500).rotate({
     angle: 0,
     animateTo: 360,
-    duration: 4500,
+    duration: 3000,
     callback: function(){
       $('.description h2').show('slide', {direction: 'right'}, 300, function(){
        $('.description p').show('slide', {direction: 'up'}, 200);
